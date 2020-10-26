697c3a24da3e8b220e089a23ec5f89a4fa0b4664
flauntingspade4 <48335751+flauntingspade4@users.noreply.github.com>
2020-10-26 14:08:00 +0000

    Change how the implementations of numerical operations are structured

--- src/polynomial.rs
+++ src/polynomial.rs
@@ -61,226 +61,232 @@ where
 
 mod impl_std_traits {
     use crate::{Polynomial, Term};
-    use core::{
-        fmt::Display,
-        ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign},
-    };
+    use core::{fmt::Display, ops::Neg};
 
     use num_traits::{Num, Pow};
+    mod div {
+        use super::{Neg, Num, Polynomial, Pow, Term};
+        use std::ops::{Div, DivAssign};
+        impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd + Neg<Output = T>> Div for Polynomial<T> {
+            type Output = Self;
 
-    impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd + Neg<Output = T>> Div for Polynomial<T>
-    where
-        Term<T>: Neg<Output = Term<T>>,
-    {
-        type Output = Self;
+            fn div(mut self, mut rhs: Self) -> Self::Output {
+                self.simplify();
+                rhs.simplify();
+                let mut to_return = Vec::new();
+                while self.0.len() != 1 {
+                    let to_push = Term::new(
+                        self.0[0].coefficient.clone(),
+                        self.0[0].exponent.clone() - rhs.0[0].exponent.clone(),
+                    );
+                    to_return.push(to_push.clone());
+                    self -= rhs.clone() * to_push;
+                }
+                Self(to_return)
+            }
+        }
+        impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd + Neg<Output = T>> DivAssign
+            for Polynomial<T>
+        {
+            fn div_assign(&mut self, mut rhs: Self) {
+                self.simplify();
+                rhs.simplify();
+                let mut to_return = Vec::new();
+                while self.0.len() != 1 {
+                    let to_push = Term::new(
+                        self.0[0].coefficient.clone(),
+                        self.0[0].exponent.clone() - rhs.0[0].exponent.clone(),
+                    );
+                    to_return.push(to_push.clone());
+                    *self -= rhs.clone() * to_push;
+                }
+                self.0 = to_return;
+            }
+        }
+        impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> Div<Term<T>> for Polynomial<T> {
+            type Output = Self;
 
-        fn div(mut self, mut rhs: Self) -> Self::Output {
-            self.simplify();
-            rhs.simplify();
-            let mut to_return = Vec::new();
-            while self.0.len() != 1 {
-                let to_push = Term::new(
-                    self.0[0].coefficient.clone(),
-                    self.0[0].exponent.clone() - rhs.0[0].exponent.clone(),
-                );
-                to_return.push(to_push.clone());
-                self -= rhs.clone() * to_push;
-            }
-            Self(to_return)
-        }
-    }
-    // TODO Make this work
-    impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd + Neg<Output = T>> DivAssign
-        for Polynomial<T>
-    {
-        fn div_assign(&mut self, mut rhs: Self) {
-            self.simplify();
-            rhs.simplify();
-            let mut to_return = Vec::new();
-            while self.0.len() != 1 {
-                let to_push = Term::new(
-                    self.0[0].coefficient.clone(),
-                    self.0[0].exponent.clone() - rhs.0[0].exponent.clone(),
-                );
-                to_return.push(to_push.clone());
-                *self -= rhs.clone() * to_push;
-            }
-            self.0 = to_return;
-        }
-    }
-    impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> Div<Term<T>> for Polynomial<T> {
-        type Output = Self;
-
-        fn div(self, rhs: Term<T>) -> Self::Output {
-            let mut to_return = Vec::with_capacity(self.0.len());
-            for term in self.0.iter() {
-                to_return.push(term.clone() / rhs.clone());
-            }
-            Self(to_return)
-        }
-    }
-    impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> DivAssign<Term<T>> for Polynomial<T> {
-        fn div_assign(&mut self, rhs: Term<T>) {
-            for term in self.0.iter_mut() {
-                *term = term.clone() / rhs.clone();
+            fn div(self, rhs: Term<T>) -> Self::Output {
+                let mut to_return = Vec::with_capacity(self.0.len());
+                for term in self.0.iter() {
+                    to_return.push(term.clone() / rhs.clone());
+                }
+                Self(to_return)
             }
         }
-    }
-    impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> Div<T> for Polynomial<T> {
-        type Output = Self;
-
-        fn div(self, rhs: T) -> Self::Output {
-            let mut to_return = Vec::with_capacity(self.0.len());
-            for term in self.0.iter() {
-                to_return.push(term.clone() / rhs.clone());
-            }
-            Self(to_return)
-        }
-    }
-    impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> DivAssign<T> for Polynomial<T> {
-        fn div_assign(&mut self, rhs: T) {
-            for term in self.0.iter_mut() {
-                *term = term.clone() / rhs.clone();
-            }
-        }
-    }
-    // Allowing Polynomial<T> to multiply by Polynomial<T>, Term<T> and T
-    impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> Mul for Polynomial<T> {
-        type Output = Self;
-
-        fn mul(self, rhs: Self) -> Self::Output {
-            let mut to_return = Vec::with_capacity(self.0.len() * rhs.0.len());
-            for term in self.0.iter() {
-                for rhs_term in rhs.0.iter() {
-                    to_return.push(term.clone() * rhs_term.clone());
+        impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> DivAssign<Term<T>> for Polynomial<T> {
+            fn div_assign(&mut self, rhs: Term<T>) {
+                for term in self.0.iter_mut() {
+                    *term = term.clone() / rhs.clone();
                 }
             }
-            Self(to_return)
         }
-    }
-    impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> MulAssign for Polynomial<T> {
-        fn mul_assign(&mut self, rhs: Self) {
-            let mut to_set = Vec::with_capacity(self.0.len() * rhs.0.len());
-            for term in self.0.iter() {
-                for rhs_term in rhs.0.iter() {
-                    to_set.push(term.clone() * rhs_term.clone());
+        impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> Div<T> for Polynomial<T> {
+            type Output = Self;
+
+            fn div(self, rhs: T) -> Self::Output {
+                let mut to_return = Vec::with_capacity(self.0.len());
+                for term in self.0.iter() {
+                    to_return.push(term.clone() / rhs.clone());
+                }
+                Self(to_return)
+            }
+        }
+        impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> DivAssign<T> for Polynomial<T> {
+            fn div_assign(&mut self, rhs: T) {
+                for term in self.0.iter_mut() {
+                    *term = term.clone() / rhs.clone();
                 }
             }
-            *self = Self(to_set)
         }
     }
-    impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> Mul<Term<T>> for Polynomial<T> {
-        type Output = Self;
+    mod mul {
+        use super::{Num, Polynomial, Pow, Term};
+        use std::ops::{Mul, MulAssign};
+        impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> Mul for Polynomial<T> {
+            type Output = Self;
 
-        fn mul(self, rhs: Term<T>) -> Self::Output {
-            let mut to_return = Vec::with_capacity(self.0.len());
-            for term in self.0.iter() {
-                to_return.push(term.clone() * rhs.clone());
-            }
-            Self(to_return)
-        }
-    }
-    impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> MulAssign<Term<T>> for Polynomial<T> {
-        fn mul_assign(&mut self, rhs: Term<T>) {
-            for term in self.0.iter_mut() {
-                *term = term.clone() * rhs.clone();
+            fn mul(self, rhs: Self) -> Self::Output {
+                let mut to_return = Vec::with_capacity(self.0.len() * rhs.0.len());
+                for term in self.0.iter() {
+                    for rhs_term in rhs.0.iter() {
+                        to_return.push(term.clone() * rhs_term.clone());
+                    }
+                }
+                Self(to_return)
             }
         }
-    }
-
-    impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> Mul<T> for Polynomial<T> {
-        type Output = Self;
-
-        fn mul(self, rhs: T) -> Self::Output {
-            let mut to_return = Vec::with_capacity(self.0.len());
-            for term in self.0.iter() {
-                to_return.push(term.clone() * rhs.clone());
-            }
-            Self(to_return)
-        }
-    }
-    impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> MulAssign<T> for Polynomial<T> {
-        fn mul_assign(&mut self, rhs: T) {
-            for term in self.0.iter_mut() {
-                *term = term.clone() * rhs.clone();
+        impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> MulAssign for Polynomial<T> {
+            fn mul_assign(&mut self, rhs: Self) {
+                let mut to_set = Vec::with_capacity(self.0.len() * rhs.0.len());
+                for term in self.0.iter() {
+                    for rhs_term in rhs.0.iter() {
+                        to_set.push(term.clone() * rhs_term.clone());
+                    }
+                }
+                *self = Self(to_set)
             }
         }
-    }
+        impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> Mul<Term<T>> for Polynomial<T> {
+            type Output = Self;
 
-    impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> Sub for Polynomial<T>
-    where
-        Term<T>: Neg<Output = Term<T>>,
-    {
-        type Output = Self;
-
-        fn sub(mut self, rhs: Self) -> Self::Output {
-            for rhs_term in rhs.0 {
-                self.0.push(-rhs_term);
+            fn mul(self, rhs: Term<T>) -> Self::Output {
+                let mut to_return = Vec::with_capacity(self.0.len());
+                for term in self.0.iter() {
+                    to_return.push(term.clone() * rhs.clone());
+                }
+                Self(to_return)
             }
-            self
         }
-    }
-    impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd + Neg<Output = T>> SubAssign for Polynomial<T>
-    where
-        Term<T>: Neg<Output = Term<T>>,
-    {
-        fn sub_assign(&mut self, rhs: Self) {
-            for rhs_term in rhs.0 {
-                self.0.push(-rhs_term);
+        impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> MulAssign<Term<T>> for Polynomial<T> {
+            fn mul_assign(&mut self, rhs: Term<T>) {
+                for term in self.0.iter_mut() {
+                    *term = term.clone() * rhs.clone();
+                }
             }
-            self.simplify();
         }
-    }
-    impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd + Neg<Output = T>> Sub<Term<T>>
-        for Polynomial<T>
-    where
-        Term<T>: Neg<Output = Term<T>>,
-    {
-        type Output = Self;
+        impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> Mul<T> for Polynomial<T> {
+            type Output = Self;
 
-        fn sub(mut self, rhs: Term<T>) -> Self::Output {
-            self.0.push(-rhs);
-
-            self.simplify();
-            self
-        }
-    }
-    impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd + Neg<Output = T>> SubAssign<Term<T>>
-        for Polynomial<T>
-    where
-        Term<T>: Neg<Output = Term<T>>,
-    {
-        fn sub_assign(&mut self, rhs: Term<T>) {
-            self.0.push(-rhs);
-            self.simplify();
-        }
-    }
-
-    impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> Add for Polynomial<T>
-    where
-        Term<T>: Neg<Output = Term<T>>,
-    {
-        type Output = Self;
-
-        fn add(mut self, rhs: Self) -> Self::Output {
-            for rhs_term in rhs.0 {
-                self.0.push(rhs_term);
+            fn mul(self, rhs: T) -> Self::Output {
+                let mut to_return = Vec::with_capacity(self.0.len());
+                for term in self.0.iter() {
+                    to_return.push(term.clone() * rhs.clone());
+                }
+                Self(to_return)
             }
-            self
         }
-    }
-    impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd + Neg<Output = T>> AddAssign for Polynomial<T>
-    where
-        Term<T>: Neg<Output = Term<T>>,
-    {
-        fn add_assign(&mut self, rhs: Self) {
-            for rhs_term in rhs.0 {
-                self.0.push(rhs_term);
+        impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> MulAssign<T> for Polynomial<T> {
+            fn mul_assign(&mut self, rhs: T) {
+                for term in self.0.iter_mut() {
+                    *term = term.clone() * rhs.clone();
+                }
             }
-            self.simplify();
         }
     }
+    mod sub {
+        use super::{Neg, Num, Polynomial, Pow, Term};
+        use std::ops::{Sub, SubAssign};
 
+        impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd + Neg<Output = T>> Sub for Polynomial<T> {
+            type Output = Self;
+
+            fn sub(mut self, rhs: Self) -> Self::Output {
+                for rhs_term in rhs.0 {
+                    self.0.push(-rhs_term);
+                }
+                self
+            }
+        }
+        impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd + Neg<Output = T>> SubAssign
+            for Polynomial<T>
+        {
+            fn sub_assign(&mut self, rhs: Self) {
+                for rhs_term in rhs.0 {
+                    self.0.push(-rhs_term);
+                }
+                self.simplify();
+            }
+        }
+        impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd + Neg<Output = T>> Sub<Term<T>>
+            for Polynomial<T>
+        {
+            type Output = Self;
+
+            fn sub(mut self, rhs: Term<T>) -> Self::Output {
+                self.0.push(-rhs);
+
+                self.simplify();
+                self
+            }
+        }
+        impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd + Neg<Output = T>> SubAssign<Term<T>>
+            for Polynomial<T>
+        {
+            fn sub_assign(&mut self, rhs: Term<T>) {
+                self.0.push(-rhs);
+                self.simplify();
+            }
+        }
+    }
+    mod add {
+        use super::{Num, Polynomial, Pow, Term};
+        use std::ops::{Add, AddAssign};
+        impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> Add for Polynomial<T> {
+            type Output = Self;
+
+            fn add(mut self, rhs: Self) -> Self::Output {
+                self.0.reserve(rhs.0.len());
+                for rhs_term in rhs.0 {
+                    self.0.push(rhs_term);
+                }
+                self
+            }
+        }
+        impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> AddAssign for Polynomial<T> {
+            fn add_assign(&mut self, rhs: Self) {
+                self.0.reserve(rhs.0.len());
+                for rhs_term in rhs.0 {
+                    self.0.push(rhs_term);
+                }
+            }
+        }
+        impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> Add<Term<T>> for Polynomial<T> {
+            type Output = Self;
+
+            fn add(mut self, rhs: Term<T>) -> Self::Output {
+                self.0.reserve(1);
+                self.0.push(rhs);
+                self
+            }
+        }
+        impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd> AddAssign<Term<T>> for Polynomial<T> {
+            fn add_assign(&mut self, rhs: Term<T>) {
+                self.0.reserve(1);
+                self.0.push(rhs);
+            }
+        }
+    }
     impl<T: Num + Pow<T, Output = T> + Clone + PartialOrd + Display> Display for Polynomial<T> {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             for term in self.0.iter() {
