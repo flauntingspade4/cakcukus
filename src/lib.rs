d7a06b643a3a20720ae22e072726508db2fb1c7b
flauntingspade4 <48335751+flauntingspade4@users.noreply.github.com>
2020-10-26 12:09:44 +0000

    New cargo feature
    A new feature called `built_in_types` that's enabled my default, and compiles the types such as `Polynomial<T>` and `Term<T>`.
    Using the library without the feature will thus far lead to just the traits to help structure what polynomial calculus can look like

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,16 @@
 //! The crate is build around the type [Term](term/struct.Term.html),
 //! which is used to represent some `Coefficient * x ^ Exponent`.
 
+#[cfg_attr(feature = "built_in_types")]
 mod polynomial;
+#[cfg_attr(feature = "built_in_types")]
 pub mod term;
+
 pub mod traits;
 
+#[cfg_attr(feature = "built_in_types")]
 pub use polynomial::Polynomial;
+#[cfg_attr(feature = "built_in_types")]
 pub use term::Term;
+
 pub use traits::Calculus;
